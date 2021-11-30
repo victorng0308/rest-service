3d3b86a1743a77e908d433483a5575927bf09472
SQWJNG <SQWJNG@N5014.slake.sssb.com>
2021-11-30 11:20:12 +0800

    First Commit

---
+++ MyFirstMavenProject/src/main/java/Cat.java
@@ -0,0 +1,31 @@
+
+public class Cat {
+	private String name;
+	private float weight;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getWeight() {
+		return weight;
+	}
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+
+
+	
+	
+	
+	
+}
---
+++ MyFirstMavenProject/src/main/java/HelloWorld.java
@@ -0,0 +1,58 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		
+		/* 1 */
+		
+		/*
+		 * scanner.nextLine(); int lenght = original.length();
+		 * Scanner scanner = new Scanner(System.in);
+		 *  System.out.println("Please enter a string:");
+		 *  String
+		 * original =
+		 * 
+		 * String reverse = ""; for (int i = lenght - 1; i >= 0; i--) { reverse =
+		 * reverse + original.charAt(i); }
+		 * 
+		 * if (original.equals(reverse)) { System.out.println("This is Palindrome"); }
+		 * else { System.out.println("not palindrome"); }
+		 */
+
+		/* 2 */
+		/*
+		 * int array[] = {1,231,12,3232,23,422,12}; Arrays.sort(array);
+		 * 
+		 * for (int i : array) { System.out.print(i + " "); }
+		 * 
+		 * System.out.println(); Scanner input = new Scanner(System.in);
+		 * 
+		 * System.out.println("Please enter a number from the list to get the index:");
+		 * int search = input.nextInt(); int index = Arrays.binarySearch(array, search);
+		 * 
+		 * if (index >= 0) { System.out.println("The number you enter is on index: " +
+		 * index); } else { System.out.println("Invalid number you enter"); }
+		 */
+
+		/* 3 */
+		/*
+		 * int i = 10; i++; System.out.println(i + " " + ++i); System.out.println(i++);
+		 * System.out.println(i);
+		 */
+
+		/* 4 */
+
+		/*
+		 * int a = 0; for (int i = 0; i <= 6; i++) {
+		 * 
+		 * for (int j = 0; j <= i; j++) { a++; System.out.print(a + " "); }
+		 * System.out.println(); }
+		 * 
+		 * 
+		 * 
+		 */
+		
+		
+		
+	}
+}
